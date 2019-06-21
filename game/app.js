659972a781b545c95545d41ac441160c0b9f8c1e
julianmjmartin <40496249+julianmjmartin@users.noreply.github.com>
2019-06-21 15:29:30 -0400

    Update app.js

--- game/app.js
+++ game/app.js
@@ -121,7 +121,7 @@ function handleInput(dt) {
             if(player.pos[1] > 400) {
                 isJumping = true; 
                 player.push({
-                   pos: player.pos[1],player.pos[0]
+                   pos: pos[1]//, pos[0]
                    sprite: new Sprite('sprites.png',
                                       [0, 117],
                                       [39, 39],
